5e0aa512b94d32b53c61473c89896226075453f6
Guillermo Grande <guillegrande@gmail.com>
2022-04-15 15:58:55 +0200

    Implement getSpecificNumberTrivia() method

--- lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -1,18 +1,31 @@
+import 'package:flutter_number_trivia/core/platform/network_info.dart';
+import 'package:flutter_number_trivia/features/number_trivia/data/data_sources/number_trivia_local_data_source.dart';
 import 'package:flutter_number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:flutter_number_trivia/core/error/failures.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
+import '../data_sources/number_trivia_remote_data_source.dart';
+
 class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
+  final NumberTriviaLocalDataSource localDataSource;
+  final NumberTriviaRemoteDataSource remoteDataSource;
+  final NetworkInfo networkInfo;
+
+  NumberTriviaRepositoryImpl(
+      {required this.localDataSource,
+      required this.remoteDataSource,
+      required this.networkInfo});
+
   @override
   Future<Either<Failure, NumberTrivia>>? getRandomNumberTrivia() {
-    // TODO: implement getRandomNumberTrivia
-    throw UnimplementedError();
+    return null;
   }
 
   @override
-  Future<Either<Failure, NumberTrivia>>? getSpecificNumberTrivia(int number) {
-    // TODO: implement getSpecificNumberTrivia
-    throw UnimplementedError();
+  Future<Either<Failure, NumberTrivia>>? getSpecificNumberTrivia(
+      int number) async {
+    networkInfo.isConnected;
+    return Right(await remoteDataSource.getSpecificNumberTrivia(number)!);
   }
 }
